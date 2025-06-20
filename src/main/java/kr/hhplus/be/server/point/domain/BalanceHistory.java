41fe8376010364f614a8d3b5f1545a01fc15f4fb
hwhan <hw03822@gmail.com>
2025-06-20 13:55:36 +0900

    feat:포인트 히스토리 내역 기능 구현

---
+++ src/main/java/kr/hhplus/be/server/point/domain/BalanceHistory.java
@@ -0,0 +1,66 @@
+package kr.hhplus.be.server.point.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BalanceHistory {
+    @Id
+    private String historyId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false)
+    private Long currentBalance;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public enum TransactionType {
+        CHARGE, PAYMENT, REFUND
+    }
+
+    // 생성자
+    public BalanceHistory(Long userId, TransactionType type, Long amount, Long currentBalance) {
+        this.historyId = UUID.randomUUID().toString();
+        this.userId = userId;
+        this.type = type;
+        this.amount = amount;
+        this.currentBalance = currentBalance;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // 정적 팩토리 메소드
+    // 잔액 충전
+    public static BalanceHistory charge(Long userId, Long amount, Long currentBalance) {
+        return new BalanceHistory(userId, TransactionType.CHARGE, amount, currentBalance);
+    }
+
+    // 결제
+    public static BalanceHistory payment(Long userId, Long amount, Long currentBalance) {
+        return new BalanceHistory(userId, TransactionType.PAYMENT, amount, currentBalance);
+    }
+
+    // 환불
+    public static BalanceHistory refund(Long userId, Long amount, Long currentBalance) {
+        return new BalanceHistory(userId, TransactionType.REFUND, amount, currentBalance);
+    }
+
+    
+}
+
---
+++ src/main/java/kr/hhplus/be/server/point/repository/BalanceHistoryJpaRepository.java
@@ -0,0 +1,12 @@
+package kr.hhplus.be.server.point.repository;
+
+import kr.hhplus.be.server.point.domain.BalanceHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BalanceHistoryJpaRepository extends JpaRepository<BalanceHistory, String> {
+    List<BalanceHistory> findByUserIdOrderByCreatedAtDesc(Long userId);
+}
